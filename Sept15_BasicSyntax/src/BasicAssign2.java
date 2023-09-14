9833fc6884e78598bd2a75845a708b541cd93361
EpicCactus94 <alimyust@gmail.com>
2023-09-14 14:58:09 -0400

    Sept 12 Second Ex

--- Sept15_BasicSyntax/src/BasicAssign2.java
+++ Sept15_BasicSyntax/src/BasicAssign2.java
@@ -11,18 +11,32 @@ public class BasicAssign2 {
             lineIn = stdin.nextLine().toUpperCase();
         }
         for (int i = 1; i < lineIn.length(); i++) {
-            if (lineIn.charAt(i) == 'B'||i == lineIn.length()-1) {
+            if (lineIn.charAt(i) == 'B') {
                 if (lineIn.charAt(i - 1) == 'W')
                     lineOut += countW + "W";
                 countW = 0;
                 countB += 1;
             }
-            if (lineIn.charAt(i) == 'W' || i == lineIn.length()-1) {
+            if (lineIn.charAt(i) == 'W') {
                 if (lineIn.charAt(i - 1) == 'B')
                     lineOut += countB + "B";
                 countB = 0;
                 countW += 1;
             }
+            if (i == lineIn.length()-1) {
+                if (countW > countB) {
+                    lineOut += countW + "W";
+                }
+                if (countB > countW) {
+                    lineOut += countB + "B";
+                }
+            }
+        }
+        if(lineOut.length() == 0){
+            if(lineIn.charAt(0) == 'W')
+                lineOut += 1 + "W";
+            if(lineIn.charAt(0) == 'B')
+                lineOut += 1 + "B";
         }
         System.out.println(lineOut);
     }
--- Sept15_BasicSyntax/src/BasicAssign4.java
+++ Sept15_BasicSyntax/src/BasicAssign4.java
@@ -5,21 +5,31 @@ public class BasicAssign4 {
     public static void main(String[] args) {
         Random rand = new Random();
         int[] birthdays = new int[366];
-        birthdays[0] = rand.nextInt(1, 365);
+        birthdays[0] = -rand.nextInt(1, 365);
         int count = 0;
-        for (int j = 0; j < 10000; j++) {
-            for (int i = 0; i < 365; i++) {
-                birthdays[i] = (int) Math.ceil(Math.random()*365);
+
+        int r = 0;
+            for (int i = 1; i < 365; i++) {
+                birthdays[i] = (int) -Math.ceil(Math.random() * 365);
                 Arrays.sort(birthdays);
-                System.out.println(Arrays.toString(birthdays));
-                if (birthdays[365-i] == birthdays[365-i-1] && birthdays[365-i] != 0) {
-                    count += i;
+                if (r >= 10000)
                     break;
-                }
-                Arrays.fill(birthdays, 0);
-                birthdays[0] = rand.nextInt(1, 365);
+//                System.out.println(r);
+                for (int j = 1; j <= i; j++) {
+                    if (birthdays[j] == birthdays[j - 1]) {
+                        count += i -1;
+                        r += 1;
+                        i = 1;
+                        if (j < 3) {
+                            System.out.println(Arrays.toString(birthdays));
+                        System.out.println(j);
+                        }
+                            Arrays.fill(birthdays, 0);
+                        birthdays[0] = -rand.nextInt(1, 365);
+                        break;
+                    }
             }
         }
-        System.out.println(count / 10000);
+        System.out.println(((double)count)/(double)10000);
     }
 }
