f7d5f15f8176edbb67eae8bfa25ed8dfb84b2bcb
EpicCactus94 <alimyust@gmail.com>
2023-11-06 14:10:48 -0500

    Nov 6 School 1

--- Nov1_AsteroidsGame/src/Meteoroid.java
+++ Nov1_AsteroidsGame/src/Meteoroid.java
@@ -5,13 +5,14 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Meteoroid extends SpaceObject{
-    private static final int[] WID = {25,75,150};
+    private static final int[] WID = {25,55,110};
     private final int BIG = 2,MED = 1, SMALL = 0;
     private final int size;
     private double rotCount;
     private double rotSpeed;
     private final double[][] BIG_PTS = {{40, 103, 173, 137, 159, 100, 74, 86, 47,40},
             {35, -12, 22, 66, 108, 84, 95, 46, 57,35}};
+//    private final double[][] BIG_PTS = {{-100, -100, 100, 100},{-100, -100, 100, 100}};
     private final double[][] SML_PTS = new double[2][BIG_PTS[0].length];
 
     private final double[][] MED_PTS = new double[2][BIG_PTS[0].length];
@@ -36,16 +37,13 @@ public class Meteoroid extends SpaceObject{
 
     public void drawMeteoroid(Graphics g) {
         System.out.println(size);
-//        System.out.println(this.x + " , " + this.y);
         rotCount = (rotCount%360 ==0)?1:rotCount+rotSpeed;
         g.setColor(Color.white);
-//        System.out.println(Arrays.deepToString(BIG_PTS));
-//        System.out.println(Arrays.deepToString(MED_PTS));
-//        System.out.println(Arrays.deepToString(SML_PTS));
-//System.out.println("---------------");
-        int[][] rotatedPts = rotatePoints(PTS[size],rotCount, this.x,this.y);;
+        int[][] rotatedPts = rotatePoints(PTS[size],rotCount,
+                this.x-WID[size]/2,this.y-WID[size]/2);
         g.drawPolygon(rotatedPts[0],rotatedPts[1], BIG_PTS[0].length);
-        g.drawOval(this.x, this.y, WID[size], WID[size]);
+        g.drawOval(this.x-WID[size]/2, this.y-WID[size]/2, WID[size], WID[size]);
+//        g.drawRect(this.x,this.y,WID[size], WID[size]);
 
     }
 
